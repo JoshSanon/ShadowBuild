3d85af68c0029ad7137adea80201d59e9409dced
JoshSanon <49575884+JoshSanon@users.noreply.github.com>
2019-04-14 13:31:07 +1000

    Piece moving

--- src/Scout.java
+++ src/Scout.java
@@ -3,16 +3,22 @@ import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 public class Scout {
-	final static double speed=0.25;
-	public float x;
-    public float y;
-
+	final static float speed=0.25f;
+	private float x;
+	private float y;
+	private float dest_x;
+	private float dest_y;   
+	private float angle;
+    
 	private Image image;
 	public Scout(float x,float y) throws SlickException
 	{
 		image=new Image("assets/scout.png");
 		this.x=x;
 		this.y=y;
+		this.dest_x=x;
+		this.dest_y=y;
+		angle=0;
 	}
 	
 	public float getX() {
@@ -23,15 +29,18 @@ public class Scout {
         return y;
     }
     public void move(Input input,int delta) {
-    	float angle;
 		if (input.isMousePressed(input.MOUSE_RIGHT_BUTTON)) {
-			angle = (float)Math.atan2(input.getMouseY()-this.y,input.getMouseX()-this.x);	
+			dest_x=input.getMouseX();
+			dest_y=input.getMouseY();
+			angle = (float)Math.atan2(dest_y-this.y,dest_x-this.x);	
 		}
-    	this.x+=(float)Math.cos(angle) *delta*speed;
-    	this.y+= (float)Math.sin(angle)*delta*speed;
+		if(Math.abs(Math.hypot(x-dest_x, y-dest_y)) >speed) {
+			angle = (float)Math.atan2(dest_y-this.y,dest_x-this.x);	
+			this.x+=(float)Math.cos(angle) *delta*speed;
+			this.y+= (float)Math.sin(angle)*delta*speed;
+		}
     }
     public void render() {
         image.drawCentered(x, y);
     }
-
 }
--- src/World.java
+++ src/World.java
@@ -10,18 +10,38 @@ import org.newdawn.slick.tiled.TiledMap;
  * These classes are provided simply as a starting point. You are not strictly required to use them.
  */
 public class World {
+	// Initializing the variable map which is and object and stores the entire map of the game
 	public TiledMap map;
+	
+	// Initializing the variable scout, which is an object and the player's piece in the game
 	public Scout scout;
+	
 	public World() throws SlickException {
+		// Creating an object of class TiledMap, map and giving it details from main.tmx file
 		map= new TiledMap("assets/main.tmx");
+		
+		// Creating the object of class Scout, scout and giving its initial coordinates on the map
 		scout=new Scout(0,0);
 	}
+	/** Update the game state for a frame.
+     * @param input The Slick object for user inputs.
+     * @param delta Time passed since last frame (milliseconds).
+     */
 	public void update(Input input, int delta) {
+		
+		// Calling the method move in the class Scout which allows player to move their piece
 		scout.move(input,delta);
 	}
 	
+	/** Render the entire screen, so it reflects the current game state.
+     * @param g The Slick graphics object, used for drawing.
+     */
 	public void render(Graphics g) {
+		
+		// Rendering the map
 		map.render(0,0);
+		
+		//Rendering the player's piece- scout
 		scout.render();
 	}
 }
